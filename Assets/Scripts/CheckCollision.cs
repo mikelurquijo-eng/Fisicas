b2277407f383ba7b24ca7b74cdca18e8edc5e019
murkri229 <mikel.urquijo@opendeusto.es>
2026-04-16 08:57:40 +0200

    Collision Methods
    Collision Methods implemented

---
+++ Assets/Scripts/CheckCollision.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class CheckCollision : MonoBehaviour
+{
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Bean") {
+            Debug.Log("Comienzo a chocar");        
+        }
+    }
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.name == "Bean")
+        {
+            Debug.Log("Estoy chocando");
+        }
+    }
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name == "Bean")
+        {
+            Debug.Log("He dejado de chocar");
+            Destroy(this.gameObject);
+        }
+    }
+}
